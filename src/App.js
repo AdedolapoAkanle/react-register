06e392e965cf5d383141314f6c9e8d6541e7e559
AdedolapoAkanle <dolapoakanle@gmail.com>
2022-10-07 16:57:16 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -14,25 +14,35 @@ const App = ({state, dispatchBioData}) => {
   return (
     <div className="App">
      <div className="container">
-     <form action="" className="form" >
-      <h1 className="header">Registration</h1>
-     <label htmlFor="first-name" className="label">FirstName: {firstName}</label>
-     <input type="text"  className="field" onChange={(e) => dispatchBioData({...state, firstName: e.target.value})} />
-     <label htmlFor="last-name" className="label">LastName: {lastName}</label>
-     <input type="text" className="field" onChange={(e) => dispatchBioData({...state, lastName: e.target.value})}/>
-     <label htmlFor="Address" className="label">Address: {address}</label>
-     <input type="text" className="field" onChange={(e) => dispatchBioData({...state, address: e.target.value})}/>
-     <label htmlFor="Age" className="label">Age:{age}</label>
-     <input type="text" className="field" onChange={(e) => dispatchBioData({...state, age: e.target.value})}/>
-     <label htmlFor="gender" className="label">Gender:{gender}</label>
-     <select className="dropdown" onChange={(e) => dispatchBioData({...state, gender: e.target.value})}>
-     <option value="male" className="value">Male</option>
-     <option value="female" className="value">Female</option>
-     </select>
-     <button className="button" onClick={clearState}>submit</button>
-     
-     </form>
-     </div>
+        {/* <div className="circle1"></div> */}
+        {/* <div className="circle2"></div> */}
+        {/* <div className="circle3"></div> */}
+        {/* <div className="circle4"></div> */}
+        <div className="blur">
+          <form action="" className="form" >
+            <h1 className="header">Registration</h1>
+            <label htmlFor="first-name" className="label">FirstName: {firstName}</label>
+            <input type="text"  className="field" onChange={(e) => dispatchBioData({...state, firstName: e.target.value})} />
+            
+            <label htmlFor="last-name" className="label">LastName: {lastName}</label>
+            <input type="text" className="field" onChange={(e) => dispatchBioData({...state, lastName: e.target.value})}/>
+            
+            <label htmlFor="Address" className="label">Address: {address}</label>
+            <input type="text" className="field" onChange={(e) => dispatchBioData({...state, address: e.target.value})}/>
+            
+            <label htmlFor="Age" className="label">Age:{age}</label>
+            <input type="text" className="field" onChange={(e) => dispatchBioData({...state, age: e.target.value})}/>
+            
+            <label htmlFor="gender" className="label">Gender:{gender}</label>
+            <select className="dropdown" onChange={(e) => dispatchBioData({...state, gender: e.target.value})}>
+              <option value="male" className="value">Male</option>
+              <option value="female" className="value">Female</option>
+            </select>
+
+            <button className="button" onClick={clearState}>submit</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
